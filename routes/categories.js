e21f9464a51acbb4d204e400250ffc81c3efb4a4
Alireza Tajik <alireza.tajik1988@gmail.com>
2023-12-17 16:55:55 +0330

    add delete for category and get method for product

--- routes/categories.js
+++ routes/categories.js
@@ -17,7 +17,7 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ message: 'category not found' });
   }
   res.status(200).send(category);
-})
+});
 
 router.post('/', async (req, res) => {
   let category = new Category({
@@ -31,6 +31,19 @@ router.post('/', async (req, res) => {
     return res.status(404).send('cannot be created');
   }
   res.send(category);
+});
+
+router.put('/:id', async (req, res) => {
+  const category = await Category.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color
+  }, {new: true});
+  if (!category) {
+    return res.status(400).send('cannot be updated');
+  }
+  res.send(category);
+
 });
 
 router.delete('/:id', (req, res) => {
