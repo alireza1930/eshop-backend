b0d814e2f282d3fd3b7c64e18174675ec9b0d3fe
A_Tajik <A_Tajik@ISC.CO.IR>
2024-01-02 16:57:28 +0330

    save user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const {User} = require('../models/user');
 const express = require('express');
+const { Category } = require('../models/category');
+const  bcrypt = require('bcryptjs')
 const router = express.Router();
 
 router.get(`/`, async (req, res) =>{
@@ -10,5 +12,26 @@ router.get(`/`, async (req, res) =>{
     } 
     res.send(userList);
 })
+
+router.post('/', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hash(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    });
+
+    user = await user.save();
+    if (!user) {
+        return res.status(404).send('User cannot be created');
+    }
+    res.send(user);
+});
 
 module.exports =router;
