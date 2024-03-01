7e26b142c36d1494444b8d4060cb810721c8cfe1
Alireza Tajik <alireza.tajik1988@gmail.com>
2024-03-01 20:20:51 +0330

    disable authentication temporary

--- helper/jwt.js
+++ helper/jwt.js
@@ -11,20 +11,21 @@ function authJwt() {
 
   }).unless({
       path: [
-        {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+        /*{url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
         {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
         {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
         //{url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
         `${api}/users/login`,
-        `${api}/users/register`,
+        `${api}/users/register`,*/
+        { url: /(.*)/ }
       ]
     }
   );
 }
 
 async function isRevoked(req, payload, done) {
-  if(!payload.isAdmin) {
-    done(null, true)
+  if (!payload.isAdmin) {
+    done(null, true);
   }
 
   done();
