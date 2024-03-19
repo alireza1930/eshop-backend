851939577a870609313aa2716a854283e02e8612
Alireza Tajik <alireza.tajik1988@gmail.com>
2024-03-19 19:02:30 +0330

    132-Submit a New Product as FormData

--- routes/products.js
+++ routes/products.js
@@ -56,7 +56,6 @@ router.post(`/`, uploadOptions.single('image'), async (req, res) => {
   if (!category) return res.status(400).send('Invalid Category');
   const file = req.file;
   if (!file) return res.status(400).send('No image in the request');
-
   const fileName = file.filename;
   const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
   let product = new Product({
